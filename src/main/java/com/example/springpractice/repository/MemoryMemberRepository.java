8c81cce7bf6d2ad3b8f99231671fa7fbc5276471
jin5eok5 <ojs258@naver.com>
2023-04-06 14:38:38 +0900

    2023-04-06

--- src/main/java/com/example/springpractice/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/springpractice/repository/MemoryMemberRepository.java
@@ -16,24 +16,38 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
+        /* member라는 id와 name를 가지는 객체에
+        id 부분을 지정 [long type 0]*/
         store.put(member.getId(), member);
+        /* store라는 해시맵에 member의 id값 : member객체
+         형태로 지정*/
         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
         return  Optional.ofNullable(store.get(id));
+        //해시맵의 key인 id를통해 회원정보를 가져오는 흐름
     }
 
     @Override
-    public Optional<com.example.springpractice.domain.Member> findByName(String name) {
+    public Optional<Member> findByName(String name) {
         return store.values().stream()
+                //.steam()해시맵의 values를 루프를 돌게함
                 .filter(member -> member.getName().equals(name))
+                /*루프를 돌면서 해시맵의 vaules의 member들의 name과
+                입력받은 name이 같은지 확인하고 필터링*/
                 .findAny();
+                // 필터링된 값을 리턴해주는 부분
+        //해시맵의 values인 Member객체를통해 회원정보를 가져오는 흐름
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void cleatStore(){
+        store.clear();
     }
 }
---
+++ src/test/java/com/example/springpractice/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.springpractice.repository;
+
+import com.example.springpractice.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void affterEach(){
+        repository.cleatStore();
+    }
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("Spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+        //result에 리턴된 member객체가 member1과 일치하는지 확인
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member member3 = new Member();
+        member3.setName("spring3");
+        repository.save(member3);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(3);
+    }
+}
