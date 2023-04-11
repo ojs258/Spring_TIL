ce2888eadf69196c76988f0b46f4a2f16f6d1cda
jin5eok5 <ojs258@naver.com>
2023-04-11 20:56:33 +0900

    2023-04-11

---
+++ src/main/java/testproject/js/controller/MemberForm.java
@@ -0,0 +1,31 @@
+package testproject.js.controller;
+
+public class MemberForm {
+    private String id;
+    private String name;
+    private String pw;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+}
