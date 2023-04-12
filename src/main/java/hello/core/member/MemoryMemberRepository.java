724aab0b0b3a4f8a4670ebdf8bf36d5036d44c6a
jin5eok5 <ojs258@naver.com>
2023-04-12 17:11:11 +0900

    2023-04-12

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    public static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+
+    }
+
+    @Override
+    public Member finById(Long memberId) {
+        return null;
+    }
+}
