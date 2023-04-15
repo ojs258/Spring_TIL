1ba3006b3f6bb4b3a7c3920aaf8b50f6fe8d9f77
oh jinseok <ojs258@naver.com>
2023-04-15 22:54:27 +0900

    2023-04-15

---
+++ src/test/java/testproject/plus/PlusApplicationTests.java
@@ -0,0 +1,13 @@
+package testproject.plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
