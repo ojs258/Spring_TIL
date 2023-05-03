fdd0ffeacae1b48e8afe29501597e9e9ce8d5f6d
jin5eok5 <ojs258@naver.com>
2023-05-03 11:17:11 +0900

    2023-05-03   오늘 강의내용 코딩

--- Online_Class_1/src/test/java/hello/core/singleton/SingletonTest.java
+++ Online_Class_1/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.order.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,4 +28,32 @@ public class SingletonTest {
         assertThat(memberService1).isNotSameAs(memberService2);
     }
     //tps 초당 객체 생성 개수 단위
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void SingletonService() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회 :  호출 할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 2. 조회 :  호출 할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1 +
+                "\nmemberService2 = " + memberService2);
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
