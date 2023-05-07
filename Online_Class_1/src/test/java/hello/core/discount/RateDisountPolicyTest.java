dc5ba24049aa5d8251381e5b36e1b4b3f56a8327
jin5eok5 <ojs258@naver.com>
2023-05-07 22:54:41 +0900

    2023-05-07    코드 리뷰 내용 추가

--- Online_Class_1/src/test/java/hello/core/discount/RateDisountPolicyTest.java
+++ Online_Class_1/src/test/java/hello/core/discount/RateDisountPolicyTest.java
@@ -22,7 +22,7 @@ class RateDisountPolicyTest {
     }
 
     @Test
-    @DisplayName("VIP는 10% 할인되어야한다.")
+    @DisplayName("VIP가 아니면 할인되지 않아야한다.")
     void vip_x(){
         //given
         Member member = new Member(2L,"memberB", Grade.BASIC);
