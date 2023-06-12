442daf7d2ada48cbdaf89ec26c637fc080532e1e
oh jinseok <ojs258@naver.com>
2023-06-12 17:17:16 +0900

    2023-06-12 SDJ 쿼리 메소드 기능 부분 시작

--- SpringDataJPA/src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
+++ SpringDataJPA/src/test/java/study/datajpa/repository/MemberJPARepositoryTest.java
@@ -1,10 +1,13 @@
 package study.datajpa.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,9 +30,35 @@ public class MemberJPARepositoryTest {
 
         //then
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
-        assertThat(findMember.getUserName()).isEqualTo(savedMember.getUserName());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
 
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCrud() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJPARepository.save(member1);
+        memberJPARepository.save(member2);
+
+        Member findMember1 = memberJPARepository.findById(member1.getId()).get();
+        Member findMember2 = memberJPARepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberJPARepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count1 = memberJPARepository.count();
+        assertThat(count1).isEqualTo(2);
+
+        memberJPARepository.delete(member1);
+        memberJPARepository.delete(member2);
+        long count2 = memberJPARepository.count();
+        assertThat(count2).isEqualTo(0);
     }
 
 }
--- SpringDataJPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ SpringDataJPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -27,9 +30,51 @@ public class MemberRepositoryTest {
 
         //then
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
-        assertThat(findMember.getUserName()).isEqualTo(savedMember.getUserName());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
 
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCrud() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count1 = memberRepository.count();
+        assertThat(count1).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long count2 = memberRepository.count();
+        assertThat(count2).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+
     }
 
 }
