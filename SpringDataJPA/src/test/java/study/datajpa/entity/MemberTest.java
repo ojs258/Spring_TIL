2d9e8d4470dc429080899e90fa2fd00b9f1447a4
jin5eok5 <ojs258@naver.com>
2023-06-15 16:52:45 +0900

    2023-06-15 Auditing & Web컨트롤러 도메인 클래스 컨버터, 페이징과 정렬

--- SpringDataJPA/src/test/java/study/datajpa/entity/MemberTest.java
+++ SpringDataJPA/src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,9 +3,12 @@ package study.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import org.aspectj.lang.annotation.RequiredTypes;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -14,6 +17,9 @@ public class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
@@ -44,7 +50,28 @@ public class MemberTest {
             System.out.println("member = " + member);
             System.out.println(" -> member.team = " + member.getTeam());
         }
+    }
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("memberA");
+        memberRepository.save(member); // PrePersist
 
+        Thread.sleep(100);
+        member.setUsername("memberB");
 
+        em.flush(); // PreUpdate
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.getCreateDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdateDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy() = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
     }
 }
