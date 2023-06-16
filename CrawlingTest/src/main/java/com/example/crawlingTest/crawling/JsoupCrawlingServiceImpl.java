08a3554bdea32e49b1e3fd8df4fc05b1575e3717
jin5eok5 <ojs258@naver.com>
2023-06-16 16:57:02 +0900

    2023-06-16 주석 추가

--- CrawlingTest/src/main/java/com/example/crawlingTest/crawling/JsoupCrawlingServiceImpl.java
+++ CrawlingTest/src/main/java/com/example/crawlingTest/crawling/JsoupCrawlingServiceImpl.java
@@ -17,40 +17,17 @@ public class JsoupCrawlingServiceImpl implements CrawlingService{
     public List<String> crawling() {
         List<String> crawlingBody = new ArrayList<>();
         Document doc = null;
-        Connection conn = Jsoup.connect(url.getUrl());
+        Connection conn = Jsoup.connect(url.getUrl()); // Naver검색 API로 얻은 News URL로 커넥션 생성
         try {
             doc = conn.get();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        crawlingBody.add(doc.getElementsByClass("media_end_head_title").html());
-        crawlingBody.add(doc.getElementsByClass("go_trans _article_content").html());
+        crawlingBody.add(doc.getElementsByClass("media_end_head_title").html()); // 기사 제목 태그를 크롤링
+        crawlingBody.add(doc.getElementsByClass("go_trans _article_content").html()); // 기사 본문 태그를 크롤링
 
 
         return crawlingBody;
     }
-
-//
-//    private WebDriver driver;
-//    public List<WebElement> detailCrawling2() {
-//        System.setProperty("webdriver.chrome.driver","C:\\dev\\chromedriver_win32\\chromedriver.exe");
-//
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--remote-allow-origins=*");
-//
-//
-//        try {
-//            driver = new ChromeDriver();
-//            Thread.sleep(1000);
-//            driver.get(url.getUrl());
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        driver.close();
-//
-//        return driver.findElements(By.className("media_end_head_title"));
-//    }
-
 }
 
