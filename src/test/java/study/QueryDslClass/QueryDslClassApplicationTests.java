78b87be56f1b8b80c3599ac0d12d9423f6aa47aa
jin5eok5 <ojs258@naver.com>
2023-06-23 14:35:55 +0900

    2023-06-23 QueryDsl강의용 프로젝트 생성

---
+++ src/test/java/study/QueryDslClass/QueryDslClassApplicationTests.java
@@ -0,0 +1,13 @@
+package study.QueryDslClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
